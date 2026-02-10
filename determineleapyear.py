8241412606ecac67a58318c5a06cb988258f82e3
Yabbb-study <2154761553@qq.com>
2026-02-10 20:33:15 +0800

    Update determineleapyear.py

--- determineleapyear.py
+++ determineleapyear.py
@@ -4,8 +4,8 @@
 year = input("请输入年份：")
 year = int(year)
 if year % 400 == 0:
-    print("是闰年")
+    print(f"{year}是闰年")
 elif year % 4 == 0 and year % 100 != 0:
-    print("是闰年")
+    print(f"{year}是闰年")
 else:
-    print("不是闰年")
+    print(f"{year}不是闰年")
